ba2a87a9c5daed7425e5a97c6551b5d29690ec0b
FlinV <flin.verdaasdonk@gmail.com>
2022-11-02 16:37:59 +0100

    Updated vut (to handle preceding changes)

--- visualization_utilities.py
+++ visualization_utilities.py
@@ -1,22 +1,30 @@
+def get_y(model):
+    y = list((model.transformed_df["y"].values))
+    return y
 
+def get_yhat(model):
+    yhat_train = list(model.predict(predict_on_test=False))
+    yhat_test = list(model.predict(predict_on_test=True))
+    yhat = yhat_train + yhat_test
+    return yhat
+
+def get_x(model):
+    x = model.get_corresponding_dts(df=model.transformed_df)
+    return x
 
 def plot_predictions(ax, model):
     # grab actual y
-    y = list((model.transformed_data["y"].values))
+    y = get_y(model)
+    
+    # grab yhat
+    yhat = get_yhat(model)
 
-    # grab predictions, and concat
-    yhat_train = list(model.predict(data=model.train_data))
-    yhat_test = list(model.predict())
-    yhat = yhat_train + yhat_test
+    ax.set_title(f"{model.name}: Prediction vs Reality")
 
-    #y = y[:-len(yhat)]
+    x = get_x(model)
 
-    ax.set_title(f"{model.name}: Prediction Error")
 
-    x = model.get_corresponding_dts(df=model.transformed_data)
-    #x = model.get_corresponding_dts(df=model.transformed_data)[:-len(yhat)]
-
-    ax.plot(x,y, label="y")
+    ax.plot(x, y, label="y")
     ax.plot(x, yhat, label="yhat")
     ax.legend()
 
@@ -25,20 +33,15 @@ def plot_predictions(ax, model):
 
 
 def plot_prediction_error(ax, model):
-
     # grab actual y
-    y = list((model.transformed_data["y"].values))
+    y = get_y(model)
 
-    # grab predictions, and concat
-    yhat_train = list(model.predict(data=model.train_data))
-    yhat_test = list(model.predict())
-    yhat = yhat_train + yhat_test
-
-    y = y[:-len(yhat)]
+    # grab yhat
+    yhat = get_yhat(model)
 
     # calculate error
     delta = [_y - _yh for _y, _yh in zip(y, yhat)]
-    x = model.get_corresponding_dts(df=model.transformed_data)[:-len(yhat)]
+    x = get_x(model)
 
 
     ax.set_title(f"{model.name}: Prediction Error")
@@ -46,11 +49,3 @@ def plot_prediction_error(ax, model):
     ax.legend()
 
     return ax
-
-    # # plot real vs predicted
-    # plt.figure()
-    # plt.title(crf.name)
-    # plt.plot(y, label="y")
-    # plt.plot(yhat, label="yhat")
-    # plt.legend()
-    # plt.show()
