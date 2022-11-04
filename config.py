11286690dfb2b6933be46a9c99d08b963cb305d6
FlinV <flin.verdaasdonk@gmail.com>
2022-11-04 14:39:40 +0100

    Added a variety of dataloaders

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 MAIN_DATA_DIR = r"C:\Users\Flin\OneDrive - TU Eindhoven\Flin\Flin\01 - Uni\00_Internship\Nokia\00_Programming\forecasting\datasets"
+DATA_CATEGORIES = ["industrial", "residential_no_pv", "residential_with_pv"]
--- training_utilities.py
+++ training_utilities.py
@@ -1,6 +1,30 @@
 from pathlib import Path
+from config import *
 
-def yield_training_fns(h, ):
-    ...
 
-def generic_yield_fns(h, subfolder, main_data_dir="")
+def generic_yield_fns(h, specific_folder_name, main_data_dir=MAIN_DATA_DIR):
+    specific_data_dir = Path(main_data_dir) / specific_folder_name
+
+    for data_category in DATA_CATEGORIES:
+        sub_data_dir = specific_data_dir / data_category
+        fns = [fn for fn in sub_data_dir.iterdir() if fn.is_file() and fn.stem.startswith(f"h={h}_")]
+
+        for fn in fns:
+            yield fn
+
+def yield_train_fns(h, main_data_dir=MAIN_DATA_DIR):
+    for fn in generic_yield_fns(h, specific_folder_name="train", main_data_dir=main_data_dir):
+        yield fn
+
+def yield_test_fns(h, main_data_dir=MAIN_DATA_DIR):
+    for fn in generic_yield_fns(h, specific_folder_name="test", main_data_dir=main_data_dir):
+        yield fn
+
+def yield_validation_fns(h, main_data_dir=MAIN_DATA_DIR):
+    for fn in generic_yield_fns(h, specific_folder_name="validation", main_data_dir=main_data_dir):
+        yield fn
+
+if __name__ == "__main__":
+
+    for fn in yield_test_fns(h=2):
+        print(Path(fn.parent.parent.stem)/  Path(fn.parent.stem)/  fn.stem)
