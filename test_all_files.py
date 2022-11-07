ce306aa75891a1d4a2ade32deaa9c6aeca73b4f2
FlinV <flin.verdaasdonk@gmail.com>
2022-11-07 10:22:31 +0100

    path arg > data path voor Models

---
+++ test_all_files.py
@@ -0,0 +1,11 @@
+import forecasting_models as models
+import data_utilities as dut
+import visualization_utilities as vut
+import logging_utilities as lut
+import evaluation_utilities as eut
+from config import *
+import time
+
+
+if __name__ == "__main__":
+    ...
